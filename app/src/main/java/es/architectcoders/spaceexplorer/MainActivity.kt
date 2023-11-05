e20b5899a466feaa6f72a3d4eacd7d2b36b3fef0
Pedro Aguilar Fernández <pajaros33@gmail.com>
2023-11-05 13:38:36 +0100

    Splash screen implemented in MainActivity.

--- app/src/main/java/es/architectcoders/spaceexplorer/MainActivity.kt
+++ app/src/main/java/es/architectcoders/spaceexplorer/MainActivity.kt
@@ -2,10 +2,15 @@ package es.architectcoders.spaceexplorer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val screenSplashNasa = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Thread.sleep(1000)
+        screenSplashNasa.setKeepOnScreenCondition { false }
     }
 }
