1294d14c53543a2c32ce25a7fdb7fe61d5f55dff
italojar <jaramilloarmijositalo@gmail.com>
2023-11-24 22:38:55 +0100

    Add mars view

---
+++ app/src/main/java/es/architectcoders/spaceexplorer/components/mars/MarsFragment.kt
@@ -0,0 +1,32 @@
+package es.architectcoders.spaceexplorer.components.mars
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import es.architectcoders.spaceexplorer.R
+
+class MarsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MarsFragment()
+    }
+
+    private lateinit var viewModel: MarsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_mars, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MarsViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/es/architectcoders/spaceexplorer/components/mars/MarsViewModel.kt
@@ -0,0 +1,7 @@
+package es.architectcoders.spaceexplorer.components.mars
+
+import androidx.lifecycle.ViewModel
+
+class MarsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
