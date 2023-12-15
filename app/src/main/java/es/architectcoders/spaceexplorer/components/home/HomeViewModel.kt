03c459916e589afa0ee1de292bae9812cb332233
italojar <jaramilloarmijositalo@gmail.com>
2023-12-15 21:53:04 +0100

    Init implement Home view

--- app/src/main/java/es/architectcoders/spaceexplorer/components/home/HomeViewModel.kt
+++ app/src/main/java/es/architectcoders/spaceexplorer/components/home/HomeViewModel.kt
@@ -1,14 +1,21 @@
 package es.architectcoders.spaceexplorer.components.home
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import es.architectcoders.domain.usecase.GetApodUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : ViewModel() {
+class HomeViewModel @Inject constructor(
+    private val getApodUseCase: GetApodUseCase
+) : ViewModel() {
 
-    init {
-        Log.i("HomeViewModel", "init")
+    fun init() {
+        viewModelScope.launch(Dispatchers.IO) {
+            getApodUseCase()
+        }
     }
 }
