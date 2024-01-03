1d34507db86026f9a02aca7097dff9ae7b66a4dc
italojar <jaramilloarmijositalo@gmail.com>
2024-01-04 00:26:55 +0100

    Add shimmer library dependency and lifecycle-runtime

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     //splash
     implementation("androidx.core:core-splashscreen:1.0.1")
     // Navigation
@@ -62,7 +63,7 @@ dependencies {
     // Fragment
     implementation("androidx.fragment:fragment-ktx:1.6.2")
     // Activity
-    implementation("androidx.activity:activity-ktx:1.8.1")
+    implementation("androidx.activity:activity-ktx:1.8.2")
     // ViewModel
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     // Corrutinas
@@ -76,6 +77,8 @@ dependencies {
     //Glide
     implementation ("com.github.bumptech.glide:glide:4.15.1")
     kapt ("com.github.bumptech.glide:compiler:4.15.1")
+    // Shimmer
+    implementation("com.facebook.shimmer:shimmer:0.5.0")
     //Test
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
