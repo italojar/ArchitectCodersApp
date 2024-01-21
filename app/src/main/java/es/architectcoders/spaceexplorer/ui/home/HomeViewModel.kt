78b8c7c90b5679ecf962629b5c4fcf646086ad6e
italojar <jaramilloarmijositalo@gmail.com>
2024-01-21 04:35:10 +0100

    Add new use cases and retry function when an error occurs

--- app/src/main/java/es/architectcoders/spaceexplorer/ui/home/HomeViewModel.kt
+++ app/src/main/java/es/architectcoders/spaceexplorer/ui/home/HomeViewModel.kt
@@ -1,37 +1,50 @@
 package es.architectcoders.spaceexplorer.ui.home
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import es.architectcoders.domain.usecase.GetApodUseCase
+import es.architectcoders.domain.usecase.GetApodsUseCase
+import es.architectcoders.domain.usecase.RequestApodUseCase
+import es.architectcoders.domain.usecase.SaveApodFavoriteUseCase
+import es.architectcoders.spaceexplorer.common.Error
+import es.architectcoders.spaceexplorer.common.toDomain
+import es.architectcoders.spaceexplorer.common.toError
 import es.architectcoders.spaceexplorer.common.toViewObject
 import es.architectcoders.spaceexplorer.model.ApodObject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val getApodUseCase: GetApodUseCase
+    private val requestApodUseCase: RequestApodUseCase,
+    private val getApodsUseCase: GetApodsUseCase,
+    private val saveApodFavoriteUseCase: SaveApodFavoriteUseCase
 ) : ViewModel() {
 
     data class UiState(
         val loading: Boolean = false,
-        val apod: ApodObject? = null,
+        val apodList: List<ApodObject> = emptyList(),
         val navigateTo: ApodObject? = null,
-        val onBackPressed: Boolean = false
+        val onBackPressed: Boolean = false,
+        val error: Error? = null
     )
 
     private val _state = MutableStateFlow(UiState())
+
     val state: StateFlow<UiState> = _state.asStateFlow()
 
     init {
         viewModelScope.launch {
-            _state.value = UiState(loading = true)
-            _state.value = UiState(apod = getApodUseCase().toViewObject())
+            _state.value = _state.value.copy(loading = true)
+            val error: Error? = requestApodUseCase()?.toError()
+            _state.update { uiState -> uiState.copy(loading = false, error = error) }
+            getApodsUseCase().collect { apodList ->
+                _state.value = _state.value.copy(error = null, apodList = apodList.map { apod -> apod.toViewObject() })
+            }
         }
     }
 
@@ -40,14 +53,26 @@ class HomeViewModel @Inject constructor(
     }
 
     fun saveApodAsFavourite(apod: ApodObject) {
-        Log.d("HomeViewModel", "saveApodAsFavourite")
+        viewModelScope.launch {
+            val error: Error? = saveApodFavoriteUseCase(apod.toDomain())?.toError()
+            _state.value = _state.value.copy(error = error)
+        }
     }
 
     fun onApodNavigationDone() {
-        _state.value = _state.value.copy(navigateTo = null)
+        _state.value = _state.value.copy(error = null, navigateTo = null)
     }
 
     fun onBackPressed() {
         _state.value = _state.value.copy(onBackPressed = true)
+    }
+
+    fun retry() {
+        viewModelScope.launch {
+            _state.value = _state.value.copy(error = null, loading = true)
+            _state.update { uiState ->
+                uiState.copy(loading = false, error = requestApodUseCase()?.toError())
+            }
+        }
     }
 }
