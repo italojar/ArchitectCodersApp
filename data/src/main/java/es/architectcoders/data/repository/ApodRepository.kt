afec7504aa52f0b42fb0721b6330972275d9e4c8
italojar <jaramilloarmijositalo@gmail.com>
2024-01-21 04:29:41 +0100

    Add local and remote data sources

--- data/src/main/java/es/architectcoders/data/repository/ApodRepository.kt
+++ data/src/main/java/es/architectcoders/data/repository/ApodRepository.kt
@@ -1,14 +1,41 @@
 package es.architectcoders.data.repository
 
+import es.architectcoders.data.source.local.ApodLocalDataSource
+import es.architectcoders.data.source.local.model.ApodData
 import es.architectcoders.data.source.network.model.ApodResponse
-import es.architectcoders.data.source.network.service.ApodService
+import es.architectcoders.data.source.network.ApodRemoteDataSource
+import es.architectcoders.data.source.network.model.Error
+import es.architectcoders.data.source.network.model.toError
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class ApodRepository @Inject constructor(
-    private val apodService: ApodService
+    private val apodLocalDataSource: ApodLocalDataSource,
+    private val apodRemoteDataSource: ApodRemoteDataSource
 ) {
+    suspend fun requestApod(): Error? = try {
+        val apod = apodRemoteDataSource.getApod()
+        if (apod != null) {
+            val apodExist = apodLocalDataSource.apodExists(apod)
+            if (!apodExist)  {
+                apodLocalDataSource.saveApod(apod)
+            }
+        }
+        null // return null if no error
+    } catch (exception: Exception) {
+        exception.toError()
+    }
 
-    suspend fun getApod(): ApodResponse? {
-        return apodService.getApod()
+    fun getApods(): Flow<List<ApodData>> {
+        return apodLocalDataSource.getApods()
+    }
+
+    suspend fun saveApodAsFavourite(apodData: ApodData): Error? = try {
+        apodLocalDataSource.saveApodAsFavourite(apodData)
+        null // return null if no error
+    } catch (exception: Exception) {
+        exception.toError()
     }
 }
