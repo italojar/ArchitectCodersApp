e835aaab864b1319168bc6bf6d4a42d9a5bd9abd
Pedro Aguilar Fernández <pajaros33@gmail.com>
2024-02-28 10:53:55 +0100

    Parameters for request Rovers changed

--- app/src/main/java/es/architectcoders/spaceexplorer/framework/server/roverServer/RoversApiClient.kt
+++ app/src/main/java/es/architectcoders/spaceexplorer/framework/server/roverServer/RoversApiClient.kt
@@ -1,6 +1,5 @@
 package es.architectcoders.spaceexplorer.framework.server.roverServer
 
-import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -9,8 +8,6 @@ interface RoversApiClient {
     @GET("mars-photos/api/v1/rovers/curiosity/photos")
     suspend fun getRovers(
         @Query("earth_date") date: String,
-        @Query("camera") camera: String,
-        @Query("page") page: Int,
         @Query("api_key") apiKey: String
-    ): Response<RoversResponse>
+    ): RoversResponse
 }
--- usecases/src/main/java/es/architectcoders/usecases/RequestRoversUseCase.kt
+++ usecases/src/main/java/es/architectcoders/usecases/RequestRoversUseCase.kt
@@ -4,10 +4,8 @@ import es.architectcoders.data.repository.RoversRepository
 import es.architectcoders.domain.Error
 import javax.inject.Inject
 
-class RequestRoversUseCase @Inject constructor(
-    private val roversRepository: RoversRepository
-){
-    suspend operator fun invoke(date: String, camera: String, page: Int, apiKey: String): Error? {
-        return roversRepository.requestRovers(date, camera, page, apiKey)
+class RequestRoversUseCase @Inject constructor(private val roversRepository: RoversRepository){
+    suspend operator fun invoke(): Error? {
+        return roversRepository.requestRovers()
     }
 }
