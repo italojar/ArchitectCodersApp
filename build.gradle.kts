34f53c58ead5e8fe2de849582d47d65cb7391f3a
italojar <jaramilloarmijositalo@gmail.com>
2024-03-03 21:16:45 +0100

    Add secrets gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,14 +7,11 @@ buildscript {
     dependencies {
         classpath(es.architectcoders.buildSrc.Libs.androidGradlePlugin)
         classpath(es.architectcoders.buildSrc.Libs.hiltGradlePlugin)
+        classpath(es.architectcoders.buildSrc.Libs.secretsGradlePlugin)
     }
 }
 plugins {
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    //hilt
-    id("com.google.dagger.hilt.android") version "2.49" apply false
-    //secrets-gradle-plugin
-    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
     //navigation-safe-args
     id("androidx.navigation.safeargs") version "2.5.3" apply false
     //kotlin
--- buildSrc/src/main/java/es/architectcoders/buildSrc/Dependencies.kt
+++ buildSrc/src/main/java/es/architectcoders/buildSrc/Dependencies.kt
@@ -3,4 +3,5 @@ package es.architectcoders.buildSrc
 object Libs {
     const val androidGradlePlugin = "com.android.tools.build:gradle:8.1.2"
     const val hiltGradlePlugin = "com.google.dagger:hilt-android-gradle-plugin:2.49"
+    const val secretsGradlePlugin = "com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1"
 }
