b370db467ff6d1f1f7b913f1e13e4c77d0c3b561
P3r1c0 <pajaros33@gmail.com>
2024-03-19 19:52:12 +0100

    Changed Gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,9 @@ buildscript {
     }
 }
 plugins {
-    id("com.android.application") version "8.1.2" apply false
+    id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.android.library") version "8.1.2" apply false
+    id("com.android.library") version "8.3.0" apply false
     //hilt
     id("com.google.dagger.hilt.android") version "2.49" apply false
     //secrets-gradle-plugin
