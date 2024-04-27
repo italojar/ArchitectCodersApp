bf7c7ed4e716afd67f994b8a1e1790b382f05868
Pedro Aguilar Fernández <pajaros33@gmail.com>
2024-04-27 20:40:22 +0200

    created appTEstShared Module and added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,6 +90,7 @@ dependencies {
     // Hilt
     implementation("com.google.dagger:hilt-android:2.49")
     testImplementation(project(":testShared"))
+    testImplementation(project(":appTestShared"))
     testImplementation(project(":data"))
     testImplementation(project(":usecases"))
     kapt("com.google.dagger:hilt-compiler:2.49")
@@ -126,6 +127,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(project(":appTestShared"))
+
 }
 
 // Allow references to generated code
